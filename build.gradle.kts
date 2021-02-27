ef95ddfaee082c9dd0a40224a702237f48727727
Andrey Kiryushin <kiryushin.andrey@gmail.com>
2021-02-27 13:21:20 +0300

    Small build script refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.bmuschko.gradle.docker.tasks.image.*
 import com.bmuschko.gradle.docker.tasks.container.*
-import com.codingfeline.buildkonfig.gradle.*
 import com.codingfeline.buildkonfig.compiler.*
 import java.io.ByteArrayOutputStream
 
@@ -26,11 +25,11 @@ application {
     mainClassName = "ticketToRide.ServerKt"
 }
 
-val ktor_version = "1.5.0"
-val serialization_version = "1.0.1"
-val kotest_version = "4.3.2"
-val kotlin_wrappers_version = "1.0.0-pre.134-kotlin-1.4.21"
-val react_version = "16.13.0"
+val ktorVersion = "1.5.0"
+val serializationVersion = "1.0.1"
+val kotestVersion = "4.3.2"
+val kotlinWrappersVersion = "1.0.0-pre.134-kotlin-1.4.21"
+val reactVersion = "16.13.0"
 val dockerImageForHeroku = "registry.heroku.com/ticketgame/web"
 
 kotlin {
@@ -53,20 +52,20 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serialization_version")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:$serialization_version")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:$serializationVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.2")
             }
         }
         val jvmMain by getting {
             dependencies {
-                implementation("org.jetbrains:kotlin-css:$kotlin_wrappers_version")
-                implementation("org.jetbrains:kotlin-css-jvm:$kotlin_wrappers_version")
-                implementation("io.ktor:ktor-server-core:$ktor_version")
-                implementation("io.ktor:ktor-server-netty:$ktor_version")
-                implementation("io.ktor:ktor-serialization:$ktor_version")
-                implementation("io.ktor:ktor-html-builder:$ktor_version")
-                implementation("io.ktor:ktor-websockets:$ktor_version")
+                implementation("org.jetbrains:kotlin-css:$kotlinWrappersVersion")
+                implementation("org.jetbrains:kotlin-css-jvm:$kotlinWrappersVersion")
+                implementation("io.ktor:ktor-server-core:$ktorVersion")
+                implementation("io.ktor:ktor-server-netty:$ktorVersion")
+                implementation("io.ktor:ktor-serialization:$ktorVersion")
+                implementation("io.ktor:ktor-html-builder:$ktorVersion")
+                implementation("io.ktor:ktor-websockets:$ktorVersion")
                 implementation("io.github.microutils:kotlin-logging:1.7.9")
                 implementation("org.slf4j:slf4j-simple:1.7.29")
             }
@@ -76,16 +75,16 @@ kotlin {
         }
         val jvmTest by getting {
             dependencies {
-                implementation("io.kotest:kotest-runner-junit5:$kotest_version")
-                implementation("io.kotest:kotest-assertions-core-jvm:$kotest_version")
-                implementation("io.kotest:kotest-property:$kotest_version")
+                implementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+                implementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+                implementation("io.kotest:kotest-property:$kotestVersion")
             }
         }
         val jsMain by getting {
             dependencies {
-                implementation(npm("react", react_version))
-                implementation(npm("react-dom", react_version))
-                implementation(npm("react-is", react_version))
+                implementation(npm("react", reactVersion))
+                implementation(npm("react-dom", reactVersion))
+                implementation(npm("react-is", reactVersion))
                 implementation(npm("@material-ui/core", "4.9.8"))
                 implementation(npm("styled-components", "5.2.0"))
                 implementation(npm("inline-style-prefixer", "6.0.0"))
@@ -109,29 +108,29 @@ kotlin {
     }
 }
 
+buildkonfig {
+    fun getGitHash(): String {
+        val versionFromEnv = System.getenv("SOURCE_VERSION")
+        if (versionFromEnv != null)
+            return versionFromEnv
+
+        val stdout = ByteArrayOutputStream()
+        exec {
+            commandLine("git", "rev-parse", "--short", "HEAD")
+            standardOutput = stdout
+        }
+        return stdout.toString().trim()
+    }
+
+    packageName = "ticketToRide"
+    defaultConfigs {
+        buildConfigField(FieldSpec.Type.STRING, "version", getGitHash())
+    }
+}
+
 tasks {
     withType<Test> {
         useJUnitPlatform()
-    }
-
-    configure<BuildKonfigExtension> {
-        fun getGitHash(): String {
-            val versionFromEnv = System.getenv("SOURCE_VERSION")
-            if (versionFromEnv != null)
-                return versionFromEnv
-
-            val stdout = ByteArrayOutputStream()
-            exec {
-                commandLine("git", "rev-parse", "--short", "HEAD")
-                standardOutput = stdout
-            }
-            return stdout.toString().trim()
-        }
-
-        packageName = "ticketToRide"
-        defaultConfigs {
-            buildConfigField(FieldSpec.Type.STRING, "version", getGitHash())
-        }
     }
 
     val devJs = named<KotlinWebpack>("jsBrowserDevelopmentWebpack")
