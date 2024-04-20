31b0f23cc232acb1fc3d172f60029396a9805419
Andrey Kiryushin <andrey.kiryushin@jetbrains.com>
2024-04-20 19:27:10 +0200

    Fix links to map tiles provider

--- client/src/main/kotlin/ticketToRide/components/map/MapTilesProvider.kt
+++ client/src/main/kotlin/ticketToRide/components/map/MapTilesProvider.kt
@@ -1,6 +1,6 @@
 package ticketToRide.components.map
 
 enum class MapTilesProvider(val provider: (x: Number, y: Number, z: Number, dpr: Number) -> String) {
-    Terrain({ x, y, z, _ -> "https://stamen-tiles.a.ssl.fastly.net/terrain/$z/$x/$y.jpg"}),
-    Watermark({ x, y, z, _ -> "https://stamen-tiles.a.ssl.fastly.net/watercolor/$z/$x/$y.jpg"})
+    Terrain({ x, y, z, _ -> "https://tiles.stadiamaps.com/tiles/stamen_terrain/$z/$x/$y.jpg" }),
+    Watermark({ x, y, z, _ -> "https://tiles.stadiamaps.com/tiles/stamen_watercolor/$z/$x/$y.jpg" })
 }
